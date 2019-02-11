18761dab7e0206900795fc0f69a8c765fd57c6cc
rs9000 <rs.dicarlo@gmail.com>
2019-02-11 11:50:06 +0100

    clean code

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,13 +53,4 @@ jQuery(document).ready(function($) {
 
         }
     );
-
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "rs9000");
-
-
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "rs9000", selector: "#ghfeed" });
-
-
 });
