cf94ff40ec886c10b606c7be62d9c63ab262a71e
rs9000 <rs.dicarlo@gmail.com>
2020-03-22 19:33:43 +0100

    Add quarantine notes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
+   window.document.onkeydown = function(e) {
+      if (e.keyCode == 27) {
+        lightbox_close();
+      }
+    }
+
+    function lightbox_open(videofile) {
+      $('#light video source').attr('src', videofile);
+      $("#light video")[0].load();
+      var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+      window.scrollTo(0, 0);
+      document.getElementById('light').style.display = 'block';
+      document.getElementById('fade').style.display = 'block';
+      lightBoxVideo.play();
+    }
+
+    function lightbox_close() {
+      var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+      document.getElementById('light').style.display = 'none';
+      document.getElementById('fade').style.display = 'none';
+      lightBoxVideo.pause();
+    }
+
+
+
 jQuery(document).ready(function($) {
 
 
